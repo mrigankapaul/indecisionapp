70a891afdaf83e621bf383b7759aa7671f41a9c6
mriganka.paul <paul.mriganka@gmail.com>
2020-12-04 12:11:54 -0500

    Using new syntax for class properties and arrow function

--- src/app.js
+++ src/app.js
@@ -3,3 +3,28 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class OldSynctax {
+    constructor() {
+        this.name = 'Mike'
+        this.getGreeting = this.getGreeting.bind(this)
+    }
+
+    getGreeting() {
+        return `Hi My name is ${this.name}`
+    }
+}
+
+const oldSynctax = new OldSynctax()
+const getGreeting = oldSynctax.getGreeting
+console.log(getGreeting())
+
+class NewSyntax {
+    name = 'Jen'
+    getGreeting = () => {
+        return `Hi My name is ${this.name}`
+    }
+}
+
+const newSyntax = new NewSyntax()
+console.log(newSyntax.getGreeting())
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import AddOption from './AddOption';
-import Action from './Action';
-import Header from './Header';
-import Options from './Options';
+import React from "react";
+import AddOption from "./AddOption";
+import Action from "./Action";
+import Header from "./Header";
+import Options from "./Options";
 
 export default class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.state = {
-      options: []
-    };
-  }
+  state = {
+    options: [],
+  };
+
   componentDidMount() {
     try {
-      const json = localStorage.getItem('options');
+      const json = localStorage.getItem("options");
       const options = JSON.parse(json);
 
       if (options) {
@@ -30,38 +24,38 @@ export default class IndecisionApp extends React.Component {
   componentDidUpdate(prevProps, prevState) {
     if (prevState.options.length !== this.state.options.length) {
       const json = JSON.stringify(this.state.options);
-      localStorage.setItem('options', json);
+      localStorage.setItem("options", json);
     }
   }
   componentWillUnmount() {
-    console.log('componentWillUnmount');
+    console.log("componentWillUnmount");
   }
-  handleDeleteOptions() {
+  handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }));
   }
-  handleDeleteOption(optionToRemove) {
+  handleDeleteOption = (optionToRemove) => {
     this.setState((prevState) => ({
-      options: prevState.options.filter((option) => optionToRemove !== option)
+      options: prevState.options.filter((option) => optionToRemove !== option),
     }));
   }
-  handlePick() {
+  handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
     alert(option);
   }
-  handleAddOption(option) {
+  handleAddOption = (option) => {
     if (!option) {
-      return 'Enter valid value to add item';
+      return "Enter valid value to add item";
     } else if (this.state.options.indexOf(option) > -1) {
-      return 'This option already exists';
+      return "This option already exists";
     }
 
     this.setState((prevState) => ({
-      options: prevState.options.concat(option)
+      options: prevState.options.concat(option),
     }));
   }
   render() {
-    const subtitle = 'Put your life in the hands of a computer';
+    const subtitle = "Put your life in the hands of a computer";
 
     return (
       <div>
@@ -75,9 +69,7 @@ export default class IndecisionApp extends React.Component {
           handleDeleteOptions={this.handleDeleteOptions}
           handleDeleteOption={this.handleDeleteOption}
         />
-        <AddOption
-          handleAddOption={this.handleAddOption}
-        />
+        <AddOption handleAddOption={this.handleAddOption} />
       </div>
     );
   }
